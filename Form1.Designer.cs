b698bd6f6433ea9eff19f0c1cb29f751f213ea94
cembaccayucel <baccacem@aol.com>
2019-10-23 12:36:32 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Ödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.btnOrnek2 = new System.Windows.Forms.Button();
+            this.btnOrnek1 = new System.Windows.Forms.Button();
+            this.btnOrnek3 = new System.Windows.Forms.Button();
+            this.btnOrnek4 = new System.Windows.Forms.Button();
+            this.BtnOrnek5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(12, 12);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(198, 20);
+            this.txtSayi1.TabIndex = 0;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(12, 38);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(198, 20);
+            this.txtSayi2.TabIndex = 0;
+            // 
+            // btnOrnek2
+            // 
+            this.btnOrnek2.Location = new System.Drawing.Point(10, 137);
+            this.btnOrnek2.Name = "btnOrnek2";
+            this.btnOrnek2.Size = new System.Drawing.Size(196, 62);
+            this.btnOrnek2.TabIndex = 1;
+            this.btnOrnek2.Text = "Ornek2";
+            this.btnOrnek2.UseVisualStyleBackColor = true;
+            this.btnOrnek2.Click += new System.EventHandler(this.BtnOrnek2_Click);
+            // 
+            // btnOrnek1
+            // 
+            this.btnOrnek1.Location = new System.Drawing.Point(10, 64);
+            this.btnOrnek1.Name = "btnOrnek1";
+            this.btnOrnek1.Size = new System.Drawing.Size(198, 67);
+            this.btnOrnek1.TabIndex = 1;
+            this.btnOrnek1.Text = "Ornek1";
+            this.btnOrnek1.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Click += new System.EventHandler(this.BtnOrnek1_Click);
+            // 
+            // btnOrnek3
+            // 
+            this.btnOrnek3.Location = new System.Drawing.Point(8, 205);
+            this.btnOrnek3.Name = "btnOrnek3";
+            this.btnOrnek3.Size = new System.Drawing.Size(198, 61);
+            this.btnOrnek3.TabIndex = 1;
+            this.btnOrnek3.Text = "Ornek3";
+            this.btnOrnek3.UseVisualStyleBackColor = true;
+            this.btnOrnek3.Click += new System.EventHandler(this.BtnOrnek3_Click);
+            // 
+            // btnOrnek4
+            // 
+            this.btnOrnek4.Location = new System.Drawing.Point(8, 272);
+            this.btnOrnek4.Name = "btnOrnek4";
+            this.btnOrnek4.Size = new System.Drawing.Size(198, 60);
+            this.btnOrnek4.TabIndex = 1;
+            this.btnOrnek4.Text = "Ornek4";
+            this.btnOrnek4.UseVisualStyleBackColor = true;
+            this.btnOrnek4.Click += new System.EventHandler(this.BtnOrnek4_Click);
+            // 
+            // BtnOrnek5
+            // 
+            this.BtnOrnek5.Location = new System.Drawing.Point(8, 338);
+            this.BtnOrnek5.Name = "BtnOrnek5";
+            this.BtnOrnek5.Size = new System.Drawing.Size(196, 52);
+            this.BtnOrnek5.TabIndex = 2;
+            this.BtnOrnek5.Text = "Ornek5";
+            this.BtnOrnek5.UseVisualStyleBackColor = true;
+            this.BtnOrnek5.Click += new System.EventHandler(this.BtnOrnek5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 402);
+            this.Controls.Add(this.BtnOrnek5);
+            this.Controls.Add(this.btnOrnek4);
+            this.Controls.Add(this.btnOrnek3);
+            this.Controls.Add(this.btnOrnek1);
+            this.Controls.Add(this.btnOrnek2);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Button btnOrnek2;
+        private System.Windows.Forms.Button btnOrnek1;
+        private System.Windows.Forms.Button btnOrnek3;
+        private System.Windows.Forms.Button btnOrnek4;
+        private System.Windows.Forms.Button BtnOrnek5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace Ödev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void BtnOrnek1_Click(object sender, EventArgs e)
+        {
+            string s1 = txtSayi1.Text;
+            string s2 = txtSayi2.Text;
+
+            int sayi1 = Convert.ToInt32(txtSayi1);
+            int sayi2 = Convert.ToInt32(txtSayi2);
+
+            int toplam = sayi1 + sayi2;
+            int fark = sayi1 - sayi2;
+            int bolumdenkalan = toplam % fark;
+            MessageBox.Show("İşlem Sonucu : " + bolumdenkalan);
+
+        }
+
+        private void BtnOrnek2_Click(object sender, EventArgs e)
+        {
+            int sayi = int.Parse(txtSayi1.Text);
+            sayi = sayi - 10;
+            sayi = sayi + 20;
+            int mod = sayi % 2;
+            sayi = mod * mod;
+            MessageBox.Show("Islem Sonucu  :" + sayi);
+
+            sayi = int.Parse(txtSayi1.Text);
+            Double sonuc = (sayi - 10 + 20) % 2;
+            MessageBox.Show("İşlem Sonucu  :" + (sonuc + sonuc));
+
+        }
+
+        private void BtnOrnek3_Click(object sender, EventArgs e)
+        {
+            int s1 = Convert.ToInt32(txtSayi1.Text);
+            int s2 = int.Parse(txtSayi2.Text);
+
+            int birincikare = s1 * s1;
+            int ikincikare = s2 * s2;
+
+            int toplam = birincikare + ikincikare;
+            int fark = birincikare - ikincikare;
+            int sonuc = toplam + fark;
+
+            MessageBox.Show("işlem sonucu : " + sonuc);
+
+
+        }
+
+        private void BtnOrnek4_Click(object sender, EventArgs e)
+        {
+            Double vize = double.Parse(txtSayi1.Text);
+            Double Final = double.Parse(txtSayi2.Text);
+
+            Double Sonuc = (vize * 0.30) + (Final * 0.70);
+            MessageBox.Show("Not Ortalaması" + Sonuc);
+
+
+        }
+
+        private void BtnOrnek5_Click(object sender, EventArgs e)
+        {
+            String isim = txtSayi1.Text;
+            string soyisim = txtSayi2.Text;
+
+            string mail = isim + "." + soyisim + "@hotmail.com";
+            MessageBox.Show(mail);
+
+
+
+
+        }
+
+    }
+
+}
+
+
